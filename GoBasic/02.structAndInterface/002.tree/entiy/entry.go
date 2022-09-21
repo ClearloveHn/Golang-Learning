7f353c3857653e4bc378e02759f6afdc5f748cb1
lhn19 <28033640@qq.com>
2022-09-21 15:42:03 +0800

    Golang面向对象-使用组合(继承)来扩展

---
+++ GoBasic/02.structAndInterface/002.tree/entiy/entry.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	tree "Golang-Learning/GoBasic/02.structAndInterface/002.tree"
+	"fmt"
+)
+
+type myTreeNode struct {
+	node *tree.Node
+}
+
+func (myNode *myTreeNode) postOrder() {
+	if myNode == nil || myNode.node == nil {
+		return
+	}
+	left := myTreeNode{myNode.node.Left}
+	left.postOrder()
+	right := myTreeNode{myNode.node.Right}
+	right.postOrder()
+	myNode.node.Print()
+}
+
+func main() {
+	var root tree.Node
+
+	root = tree.Node{Value: 3}
+	root.Left = &tree.Node{}
+	root.Right = &tree.Node{5, nil, nil}
+	root.Right.Left = new(tree.Node)
+	root.Left.Right = &tree.Node{Value: 2}
+	root.Right.Left.SetValue(4)
+
+	root.Traverse()
+
+	fmt.Println("==============")
+	myRoot := myTreeNode{&root}
+	myRoot.postOrder()
+
+}
---
+++ GoBasic/02.structAndInterface/002.tree/node.go
@@ -0,0 +1,16 @@
+package tree
+
+import "fmt"
+
+type Node struct {
+	Value       int
+	Left, Right *Node
+}
+
+func (node Node) Print() {
+	fmt.Println(node.Value)
+}
+
+func (node *Node) SetValue(value int) {
+	node.Value = value
+}
