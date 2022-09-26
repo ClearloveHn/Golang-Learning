22ca547bd9436dc4cf1057ea7e7de8a76ab589f3
lhn19 <28033640@qq.com>
2022-09-26 09:42:07 +0800

    创建一个Goroutine

--- GoBasic/04.goroutine/001.goroutineStart/main.go
+++ GoBasic/04.goroutine/001.goroutineStart/main.go
@@ -5,13 +5,14 @@ import (
 	"time"
 )
 
-func main() {
-	for i := 0; i < 10; i++ {
-		go func(i int) {
-			for {
-				fmt.Printf("Hello from"+"goroutine %d\n", i)
-			}
-		}(i)
+func showMsg(msg string) {
+	for i := 0; i < 5; i++ {
+		fmt.Printf("msg: %s\n", msg)
+		time.Sleep(time.Millisecond * 100)
 	}
-	time.Sleep(time.Millisecond)
+}
+
+func main() {
+	go showMsg("Golang")
+	time.Sleep(time.Millisecond * 2000)
 }
